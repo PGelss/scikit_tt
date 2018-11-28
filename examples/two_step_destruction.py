494ff34c87e28eec03e642fe3e228fabc3699306
Patrick Gelss <p.gelss@fu-berlin.de>
2018-11-28 15:53:06 +0100

    revised code

--- examples/two_step_destruction.py
+++ examples/two_step_destruction.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+This is an example for the application of the QTT format to Markovian master equations of chemical reaction
+networks. For more details, see [1]_.
+
+References
+----------
+..[1] P. Gelß. "The Tensor-Train Format and Its Applications: Modeling and Analysis of Chemical Reaction
+      Networks, Catalytic Processes, Fluid Flows, and Brownian Dynamics", Freie Universität Berlin, 2017
+"""
+
 import numpy as np
 import scikit_tt.tt as tt
 import models as mdl
 import solvers.ODE as ODE
 import subfunctions as sf
 import tools.tools as tls
-
-# This is an example for the application of the QTT format to chemical reaction networks.
-# Reference: P. Gelß. "The Tensor-Train Format and Its Applications: Modeling and Analysis of Chemical
-# Reaction Networks, Catalytic Processes, Fluid Flows, and Brownian Dynamics", Freie Universität Berlin, 2017
+import matplotlib.pyplot as plt
 
 # parameters
+# ----------
 
 m = 3
 step_sizes = [0.001] * 100 + [0.1] * 9 + [1] * 9
+qtt_rank = 10
+max_rank = 30
 
-# operator
+# construct operator in TT format and convert to QTT format
+# ---------------------------------------------------------
 
 operator = mdl.two_step_destruction(1, 2, m).tt2qtt([[2] * m] + [[2] * (m + 1)] + [[2] * m] + [[2] * m],
                                                     [[2] * m] + [[2] * (m + 1)] + [[2] * m] + [[2] * m],
                                                     threshold=10 ** -14)
 
-# initial distribution
+# initial distribution in TT format and convert to QTT format
+# -----------------------------------------------------------
 
 initial_distribution = tt.TT.zeros([2 ** m, 2 ** (m + 1), 2 ** m, 2 ** m], [1] * 4)
 initial_distribution.cores[0][0, -1, 0, 0] = 1
@@ -31,24 +43,46 @@ initial_distribution.cores[1][0, -2, 0, 0] = 1
 initial_distribution.cores[2][0, 0, 0, 0] = 1
 initial_distribution.cores[3][0, 0, 0, 0] = 1
 initial_distribution = tt.TT.tt2qtt(initial_distribution, [[2] * m] + [[2] * (m + 1)] + [[2] * m] + [[2] * m],
-                                    [[1] * m] + [[1] * (m + 1)] + [[1] * m] + [[1] * m], threshold=10 ** -14)
+                                    [[1] * m] + [[1] * (m + 1)] + [[1] * m] + [[1] * m], threshold=0)
 
-# initial guess
+# initial guess in QTT format
+# ---------------------------
 
-initial_guess = tt.TT.uniform([2] * (4 * m + 1), 10).ortho_right()
+initial_guess = tt.TT.uniform([2] * (4 * m + 1), ranks=qtt_rank).ortho_right()
 
-# solve ODE
+# solve Markovian master equation in QTT format
+# ---------------------------------------------
 
+print('\nQTT approach')
+print('------------\n')
 with tls.Timer() as time:
     solution, errors = ODE.implicit_euler_mals(operator, initial_distribution, initial_guess, step_sizes,
-                                               threshold=10 ** -7, compute_errors=True)
-
-print('\n\n' + 'CPU time     : ' + str("%.2f" % time.elapsed) + 's')
-print('Maximum error: ' + str("%.2e" % np.amax(errors)))
+                                               threshold=1e-7, max_rank=max_rank, compute_errors=True)
+print('CPU time ' + '.' * 22 + ' ' + str("%.2f" % time.elapsed) + 's')
+print('Maximum error ' + '.' * 17 + ' ' + str("%.2e" % np.amax(errors)))
 
 # convert to TT and compute mean concentrations
+# ---------------------------------------------
 
 for i in range(len(solution)):
-    solution[i] = tt.TT.qtt2tt(solution[i], [m - 1, 2 * m, 3 * m, 4 * m])
+    solution[i] = tt.TT.qtt2tt(solution[i], [m, m + 1, m, m])
+mean = sf.mean_concentrations(solution)
 
-mean = sf.mean_concentrations(solution, time_steps=np.insert(np.cumsum(step_sizes), 0, 0))
+# plot mean concentrations
+# ------------------------
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams.update({'font.size': 14})
+plt.rcParams.update({'figure.autolayout': True})
+plt.plot(np.insert(np.cumsum(step_sizes),0,0), mean)
+plt.title('Mean concentrations', y=1.05)
+plt.xlabel(r'$t$')
+plt.ylabel(r'$\overline{x_i}(t)$')
+plt.axis([0, 2, 0, 2**(m+1)-2])
+plt.grid(which='major')
+plt.legend(['species ' + str(i) for i in range(1, 5)], loc=1)
+plt.show()
+
+
