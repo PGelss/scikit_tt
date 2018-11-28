124bfc68258224a98674792a81dbc3fffa0fad2e
Patrick Gelss <p.gelss@fu-berlin.de>
2018-11-28 14:32:14 +0100

    revised trapezoidal_rule_als

--- solvers/ODE.py
+++ solvers/ODE.py
@@ -5,6 +5,7 @@ import sys
 import solvers.SLE as SLE
 import scikit_tt.tt as tt
 import numpy as np
+import tools.tools as tls
 
 def adaptive_als(operator, initial_value, initial_guess, first_step, T_end, repeats=1, loc_tol = 10**-3, tau_max = 10, max_factor = 1.25, safety_factor = 0.9, solver='lu', compute_errors=False):
     solution = [None]
@@ -97,19 +98,60 @@ def implicit_euler_mals(operator, initial_value, initial_guess, step_sizes, repe
     else:
         return solution
 
-def trapezoidal_rule_als(operator, initial_value, initial_guess, step_sizes, repeats = 1, solver='solve', compute_errors=False):
 
-    solution=[None]
-    solution[0] = initial_value
-    X = initial_guess
+def trapezoidal_rule_als(operator, initial_value, initial_guess, step_sizes, repeats = 1, solver='solve', compute_errors=False):
+    """Trapezoidal rule for linear differential equations in the TT format using ALS
+
+    Parameters
+    ----------
+    operator: instance of TT class
+        TT operator of the differential equation
+    initial_value: instance of TT class
+        initial value of the differential equation
+    initial guess: instance of TT class
+        initial guess for the first step
+    step_sizes: list of floats
+        step sizes for the application of the trapezoidal rule
+    repeats: int, optional
+        number of repeats of the ALS in each iteration step
+    solver: string, optional
+        algorithm for obtaining the solutions of the micro systems, can be 'solve' or 'lu', default is 'solve'
+    compute_errors: bool, optional
+        whether to compute the relative errors of the systems of linear equations in each step, default is False
+
+    Returns
+    -------
+    solution: list of instances of the TT class
+        numerical solution of the differential equation
+    """
+
+    # define solution
+    solution = [initial_value]
+
+    # define temporary tensor train
+    tt_tmp = initial_guess
+
+    # define errors
     errors = []
+
+    # begin trapezoidal rule
+    # ----------------------
 
     for i in range(len(step_sizes)):
 
-        X = SLE.als(tt.TT.eye(operator.row_dims) - 0.5*step_sizes[i] * operator, X, (tt.TT.eye(operator.row_dims) + 0.5*step_sizes[i] * operator) @ solution[i], solver=solver, repeats=repeats)
-        X = (1 / X.norm(p=1)) * X
-        solution.append(X.copy())
-        sys.stdout.write('\r' + 'Running trapezoidal_rule_als... ' + str(int(1000 * (i + 1) / len(step_sizes)) / 10) + '%')
+        # solve system of linear equations for current time step
+        tt_tmp = SLE.als(tt.TT.eye(operator.row_dims) - 0.5*step_sizes[i] * operator, tt_tmp, (tt.TT.eye(operator.row_dims) + 0.5*step_sizes[i] * operator) @ solution[i], solver=solver, repeats=repeats)
+
+        # normalize solution
+        tt_tmp = (1 / tt_tmp.norm(p=1)) * tt_tmp
+
+        # append solution
+        solution.append(tt_tmp.copy())
+
+        # print progress
+        tls.progress('Running trapezoidal_rule_als',100*i/(len(step_sizes)-1))
+
+        # compute error and append
         if compute_errors:
             errors.append(((tt.TT.eye(operator.row_dims) - 0.5 * step_sizes[i] * operator)@solution[i+1]-(tt.TT.eye(operator.row_dims) + 0.5 * step_sizes[i] * operator)@solution[i]).norm()/((tt.TT.eye(operator.row_dims) + 0.5 * step_sizes[i] * operator)@solution[i]).norm())
 
