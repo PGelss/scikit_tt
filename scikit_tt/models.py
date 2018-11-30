e57494c1fbd4bf5112e854d7506c7fb6fbe08480
Patrick Gelss <p.gelss@fu-berlin.de>
2018-11-30 15:57:01 +0100

    revised code

--- scikit_tt/models.py
+++ scikit_tt/models.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import scikit_tt as tt
-from scikit_tt import SLIM
+from scikit_tt.tensor_train import TT
+import scikit_tt.slim as slim
 
 
 def signaling_cascade(d):
@@ -29,33 +29,32 @@ def signaling_cascade(d):
     """
 
     # define core elements
-    S_star = 0.7 * (np.eye(64, k=-1) - np.eye(64)) + 0.07 * (np.eye(64, k=1) - np.eye(64)) @ np.diag(np.arange(64))
-    S = 0.07 * (np.eye(64, k=1) - np.eye(64)) @ np.diag(np.arange(64))
-    L = np.diag(np.arange(64)) @ np.diag(np.reciprocal(np.arange(5.0, 69.0)))
-    I = np.eye(64)
-    M = np.eye(64, k=-1) - np.eye(64)
+    s_star = 0.7 * (np.eye(64, k=-1) - np.eye(64)) + 0.07 * (np.eye(64, k=1) - np.eye(64)) @ np.diag(np.arange(64))
+    s = 0.07 * (np.eye(64, k=1) - np.eye(64)) @ np.diag(np.arange(64))
+    l = np.diag(np.arange(64)) @ np.diag(np.reciprocal(np.arange(5.0, 69.0)))
+    i = np.eye(64)
+    m = np.eye(64, k=-1) - np.eye(64)
 
     # define TT cores
-    cores = [None] * d
-    cores[0] = np.zeros([1, 64, 64, 3])
-    cores[0][0, :, :, 0] = S_star
-    cores[0][0, :, :, 1] = L
-    cores[0][0, :, :, 2] = I
-    cores[1] = np.zeros([3, 64, 64, 3])
-    cores[1][0, :, :, 0] = I
-    cores[1][1, :, :, 0] = M
-    cores[1][2, :, :, 0] = S
-    cores[1][2, :, :, 1] = L
-    cores[1][2, :, :, 2] = I
-    for i in range(2, d - 1):
-        cores[i] = cores[1]
-    cores[d - 1] = np.zeros([3, 64, 64, 1])
-    cores[d - 1][0, :, :, 0] = I
-    cores[d - 1][1, :, :, 0] = M
-    cores[d - 1][2, :, :, 0] = S
+    cores = [np.zeros([1, 64, 64, 3])]
+    cores[0][0, :, :, 0] = s_star
+    cores[0][0, :, :, 1] = l
+    cores[0][0, :, :, 2] = i
+    cores.append(np.zeros([3, 64, 64, 3]))
+    cores[1][0, :, :, 0] = i
+    cores[1][1, :, :, 0] = m
+    cores[1][2, :, :, 0] = s
+    cores[1][2, :, :, 1] = l
+    cores[1][2, :, :, 2] = i
+    for k in range(2, d - 1):
+        cores.append(cores[1])
+    cores.append(np.zeros([3, 64, 64, 1]))
+    cores[d - 1][0, :, :, 0] = i
+    cores[d - 1][1, :, :, 0] = m
+    cores[d - 1][2, :, :, 0] = s
 
     # define TT operator
-    operator = tt.TT(cores)
+    operator = TT(cores)
 
     return operator
 
@@ -91,9 +90,7 @@ def two_step_destruction(k_1, k_2, m):
     r = [1, 3, 5, 3, 1]
 
     # define TT cores
-    cores = [None] * 4
-    for i in range(4):
-        cores[i] = np.zeros([r[i], n[i], n[i], r[i + 1]])
+    cores = [np.zeros([r[i], n[i], n[i], r[i + 1]]) for i in range(4)]
     cores[0][0, :, :, 0] = np.eye(n[0])
     cores[0][0, :, :, 1] = k_1 * np.eye(n[0], k=1) @ np.diag(np.arange(n[0]))
     cores[0][0, :, :, 2] = -k_1 * np.diag(np.arange(n[0]))
@@ -112,12 +109,12 @@ def two_step_destruction(k_1, k_2, m):
     cores[3][2, :, :, 0] = np.eye(n[3])
 
     # define operator
-    operator = tt.TT(cores)
+    operator = TT(cores)
 
     return operator
 
 
-def co_oxidation(order, k_ad_CO):
+def co_oxidation(order, k_ad_co):
     """"Co oxidation on RuO2
 
     Model for the CO oxidation on a RuO2 surface. For a detailed description of the process and the construction of the
@@ -127,7 +124,7 @@ def co_oxidation(order, k_ad_CO):
     ---------
     order: int
         number of reaction sites (= order of the operator)
-    k_ad_CO: float
+    k_ad_co: float
         reaction rate constant for the adsorption of CO
 
     Returns
@@ -147,12 +144,12 @@ def co_oxidation(order, k_ad_CO):
     """
 
     # define reaction rate constants
-    k_ad_O2 = 9.7e7
-    k_de_CO = 9.2e6
-    k_de_O2 = 2.8e1
-    k_diff_CO = 6.6e-2
-    k_diff_O = 5.0e-1
-    k_de_CO2 = 1.7e5
+    k_ad_o2 = 9.7e7
+    k_de_co = 9.2e6
+    k_de_o2 = 2.8e1
+    k_diff_co = 6.6e-2
+    k_diff_o = 5.0e-1
+    k_de_co2 = 1.7e5
 
     # define state space
     state_space = [3] * order
@@ -161,15 +158,12 @@ def co_oxidation(order, k_ad_CO):
     # ------------------------------------------------------------------
 
     # define list of reactions
-    single_cell_reactions = [None] * order
-    two_cell_reactions = [None] * order
-    for i in range(order):
-        single_cell_reactions[i] = [[0, 2, k_ad_CO], [2, 0, k_de_CO]]
-        two_cell_reactions[i] = [[0, 1, 0, 1, k_ad_O2], [1, 0, 1, 0, k_de_O2], [2, 0, 1, 0, k_de_CO2],
-                                 [1, 0, 2, 0, k_de_CO2], [1, 0, 0, 1, k_diff_O], [0, 1, 1, 0, k_diff_O],
-                                 [0, 2, 2, 0, k_diff_CO], [2, 0, 0, 2, k_diff_CO]]
+    single_cell_reactions = [[[0, 2, k_ad_co], [2, 0, k_de_co]]] * order
+    two_cell_reactions = [[[0, 1, 0, 1, k_ad_o2], [1, 0, 1, 0, k_de_o2], [2, 0, 1, 0, k_de_co2],
+                           [1, 0, 2, 0, k_de_co2], [1, 0, 0, 1, k_diff_o], [0, 1, 1, 0, k_diff_o],
+                           [0, 2, 2, 0, k_diff_co], [2, 0, 0, 2, k_diff_co]]] * order
 
     # define operator
-    operator = SLIM.slim_mme(state_space, single_cell_reactions, two_cell_reactions)
+    operator = slim.slim_mme(state_space, single_cell_reactions, two_cell_reactions)
 
     return operator
