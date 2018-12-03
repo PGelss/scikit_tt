41a4da41c1a5b3e8efe921f91e6697b6c12a3a28
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-03 13:38:00 +0100

    merged subfunctions.py and tools.py

--- scikit_tt/subfunctions.py
+++ scikit_tt/subfunctions.py
@@ -2,7 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import time as _time
+import sys
 from scikit_tt.tensor_train import TT
+
+
+class Timer(object):
+    """Measure CPU time"""
+
+    def __enter__(self):
+        self.start_time = _time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.elapsed = _time.time() - self.start_time
+
+
+def progress(text, percent, dots=3):
+    """Show progress in percent"""
+
+    sys.stdout.write('\r' + text + ' ' + dots * '.' + ' ' + str("%.1f" % percent) + '%')
+
+    if percent == 100:
+        sys.stdout.write('\n')
 
 
 def unit_vector(dimension, index):
@@ -59,6 +81,6 @@ def mean_concentrations(series):
             tensor_mean = TT(cores)
 
             # define entry of mean
-            mean[i, j] = (series[i].transpose() @ tensor_mean).element([0]*2*series[0].order)
+            mean[i, j] = (series[i].transpose() @ tensor_mean).element([0] * 2 * series[0].order)
 
     return mean
