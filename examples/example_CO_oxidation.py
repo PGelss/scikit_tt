a7012bc38861933db2bce1b01f4fd55c237d6510
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-03 09:52:15 +0100

    revised code

--- examples/example_CO_oxidation.py
+++ examples/example_CO_oxidation.py
@@ -5,7 +5,7 @@ import scikit_tt.tensor_train as tt
 import scikit_tt.models as mdl
 import scikit_tt.solvers.EVP as evp
 import scikit_tt.solvers.ODE as ODE
-import tools as tls
+import scikit_tt.tools as tls
 import matplotlib.pyplot as plt
 
 
@@ -40,29 +40,30 @@ print('\n----------------------------------------------------------')
 print('p_CO in atm    Method    TT ranks    Closeness    CPU time')
 print('----------------------------------------------------------')
 
-# R = [3,3,4,7,8,8,10,10]
-# for i in range(len(R)):
-#     operator = mdl.CO_oxidation(20, 10 ** (8 + p_CO_exp[i]))
-#     initial_guess = tt.TT.ones(operator.row_dims,[1]*operator.order, ranks=R[i]).ortho_right()
-#     with tls.Timer() as time:
-#         solution, eigenvalues = evp.als(tt.TT.eye(operator.row_dims)+operator, initial_guess, number_ev=1, repeats=20)
-#         solution = (1/solution.norm(p=1))*solution
-#     TOF.append(turn_over_frequency(solution).copy())
-#     print('10^'+(p_CO_exp[i]>=0)*'+'+str("%.1f" % p_CO_exp[i])+'         EVP         '+(R[i]<10)*' '+str(R[i])+'        '+str("%.2e" % (operator@solution).norm())+(time.elapsed<10)*' '+'      '+str("%.2f" % time.elapsed) + 's')
-
-R = [14, 15, 17, 18]
-for i in range(3,len(R)):
-    operator = mdl.CO_oxidation(20, 10 ** (8 + p_CO_exp[8+i]))
-    initial_value = tt.TT.zeros(operator.row_dims, [1] * operator.order)
-    for j in range(initial_value.order):
-        initial_value.cores[j][0,1,0,0] = 1
-    initial_guess = tt.TT.ones(operator.row_dims, [1] * operator.order, ranks=R[i]).ortho_right()
+R = [3,3,4,7,8,8,10,10]
+for i in range(len(R)):
+    operator = mdl.co_oxidation(20, 10 ** (8 + p_CO_exp[i]))
+    initial_guess = tt.TT.ones(operator.row_dims,[1]*operator.order, ranks=R[i]).ortho_right()
     with tls.Timer() as time:
-        solution = ODE.adaptive_als(operator, initial_value, initial_guess, 10**-10, 10, repeats = 5)
-    TOF.append(turn_over_frequency(solution[-1]).copy())
-    print('10^+' + str("%.1f" % p_CO_exp[i+8]) + '         IEM         ' + (R[i] < 10) * ' ' + str(
-        R[i]) + '        ' + str("%.2e" % (operator @ solution[-1]).norm()) + (time.elapsed < 10) * ' ' + '      ' + str(
-        "%.2f" % time.elapsed) + 's')
+        solution, eigenvalues = evp.als(tt.TT.eye(operator.row_dims)+operator, initial_guess, number_ev=1, repeats=20)
+        print(eigenvalues)
+        solution = (1/solution.norm(p=1))*solution
+    TOF.append(turn_over_frequency(solution).copy())
+    print('10^'+(p_CO_exp[i]>=0)*'+'+str("%.1f" % p_CO_exp[i])+'         EVP         '+(R[i]<10)*' '+str(R[i])+'        '+str("%.2e" % (operator@solution).norm())+(time.elapsed<10)*' '+'      '+str("%.2f" % time.elapsed) + 's')
+
+# R = [14, 15, 17, 18]
+# for i in range(3,len(R)):
+#     operator = mdl.CO_oxidation(20, 10 ** (8 + p_CO_exp[8+i]))
+#     initial_value = tt.TT.zeros(operator.row_dims, [1] * operator.order)
+#     for j in range(initial_value.order):
+#         initial_value.cores[j][0,1,0,0] = 1
+#     initial_guess = tt.TT.ones(operator.row_dims, [1] * operator.order, ranks=R[i]).ortho_right()
+#     with tls.Timer() as time:
+#         solution = ODE.adaptive_als(operator, initial_value, initial_guess, 10**-10, 10, repeats = 5)
+#     TOF.append(turn_over_frequency(solution[-1]).copy())
+#     print('10^+' + str("%.1f" % p_CO_exp[i+8]) + '         IEM         ' + (R[i] < 10) * ' ' + str(
+#         R[i]) + '        ' + str("%.2e" % (operator @ solution[-1]).norm()) + (time.elapsed < 10) * ' ' + '      ' + str(
+#         "%.2f" % time.elapsed) + 's')
 
 print('------------------------------------------------')
 
