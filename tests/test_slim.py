51b751f3eaf128507fc02ad3cbcba10ea1069021
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-05 17:35:54 +0100

    revised code

--- tests/test_slim.py
+++ tests/test_slim.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import scipy.linalg as linalg
-import scikit_tt.models as mdl
 from unittest import TestCase
 from scikit_tt.tensor_train import TT
+import scikit_tt.models as mdl
+import numpy as np
 
 
 class TestSLIM(TestCase):
@@ -36,49 +35,51 @@ class TestSLIM(TestCase):
 
         # define core elements
         s_mat = np.array([[-self.k_ad_co, 0, k_de_co], [0, 0, 0], [self.k_ad_co, 0, -k_de_co]])
-        l_mat = [np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]])]
-        l_mat.append(np.array([[0, 1, 0], [0, 0, 0], [0, 0, 0]]))
-        l_mat.append(np.array([[0, 0, 1], [0, 0, 0], [0, 0, 0]]))
-        l_mat.append(np.array([[0, 0, 0], [1, 0, 0], [0, 0, 0]]))
-        l_mat.append(np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
-        l_mat.append(np.array([[0, 0, 0], [0, 0, 0], [1, 0, 0]]))
-        l_mat.append(np.array([[0, 0, 0], [0, 0, 0], [0, 0, 1]]))
-        m_mat = [np.array([[-k_ad_o2, 0, 0], [0, -k_diff_o, 0], [0, 0, -k_diff_co]])]
-        m_mat.append(np.array([[0, k_de_o2, k_de_co2], [k_diff_o, 0, 0], [0, 0, 0]]))
-        m_mat.append(np.array([[0, k_de_co2, 0], [0, 0, 0], [k_diff_co, 0, 0]]))
-        m_mat.append(np.array([[0, k_diff_o, 0], [k_ad_o2, 0, 0], [0, 0, 0]]))
-        m_mat.append(np.array([[-k_diff_o, 0, 0], [0, -k_de_o2, 0], [0, 0, -k_de_co2]]))
-        m_mat.append(np.array([[0, 0, k_diff_co], [0, 0, 0], [0, 0, 0]]))
-        m_mat.append(np.array([[-k_diff_co, 0, 0], [0, -k_de_co2, 0], [0, 0, 0]]))
+        l_mat = [None, None, None, None, None, None, None]
+        l_mat[0] = np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]])
+        l_mat[1] = np.array([[0, 1, 0], [0, 0, 0], [0, 0, 0]])
+        l_mat[2] = np.array([[0, 0, 1], [0, 0, 0], [0, 0, 0]])
+        l_mat[3] = np.array([[0, 0, 0], [1, 0, 0], [0, 0, 0]])
+        l_mat[4] = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+        l_mat[5] = np.array([[0, 0, 0], [0, 0, 0], [1, 0, 0]])
+        l_mat[6] = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 1]])
+        m_mat = [None, None, None, None, None, None, None]
+        m_mat[0] = np.array([[-k_ad_o2, 0, 0], [0, -k_diff_o, 0], [0, 0, -k_diff_co]])
+        m_mat[1] = np.array([[0, k_de_o2, k_de_co2], [k_diff_o, 0, 0], [0, 0, 0]])
+        m_mat[2] = np.array([[0, k_de_co2, 0], [0, 0, 0], [k_diff_co, 0, 0]])
+        m_mat[3] = np.array([[0, k_diff_o, 0], [k_ad_o2, 0, 0], [0, 0, 0]])
+        m_mat[4] = np.array([[-k_diff_o, 0, 0], [0, -k_de_o2, 0], [0, 0, -k_de_co2]])
+        m_mat[5] = np.array([[0, 0, k_diff_co], [0, 0, 0], [0, 0, 0]])
+        m_mat[6] = np.array([[-k_diff_co, 0, 0], [0, -k_de_co2, 0], [0, 0, 0]])
         i_mat = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
         # construct TT cores
-        cores=[np.zeros([1,3,3,16])]
-        cores[0][0,:,:,0] = s_mat
+        cores = [np.zeros([1, 3, 3, 16])]
+        cores[0][0, :, :, 0] = s_mat
         for i in range(7):
-            cores[0][0,:,:,1+i]=l_mat[i]
-        cores[0][0,:,:,8]=i_mat
+            cores[0][0, :, :, 1 + i] = l_mat[i]
+        cores[0][0, :, :, 8] = i_mat
         for i in range(7):
-        	cores[0][0,:,:,9+i]=m_mat[i]
+            cores[0][0, :, :, 9 + i] = m_mat[i]
 
-        for k in range(1,self.order-1):
-            cores.append(np.zeros([16,3,3,16]))
-            cores[k][0,:,:,0] = i_mat
+        for k in range(1, self.order - 1):
+            cores.append(np.zeros([16, 3, 3, 16]))
+            cores[k][0, :, :, 0] = i_mat
             for i in range(7):
-                cores[k][1+i,:,:,0]=m_mat[i]
-            cores[k][8,:,:,0]=s_mat
+                cores[k][1 + i, :, :, 0] = m_mat[i]
+            cores[k][8, :, :, 0] = s_mat
             for i in range(7):
-                cores[k][8,:,:,1+i]=l_mat[i]
-            cores[k][8,:,:,8] = i_mat
+                cores[k][8, :, :, 1 + i] = l_mat[i]
+            cores[k][8, :, :, 8] = i_mat
             for i in range(7):
-            	cores[k][9+i,:,:,9+i] = i_mat
-        cores.append(np.zeros([16,3,3,1]))
-        cores[-1][0,:,:,0]=i_mat
+                cores[k][9 + i, :, :, 9 + i] = i_mat
+        cores.append(np.zeros([16, 3, 3, 1]))
+        cores[-1][0, :, :, 0] = i_mat
         for i in range(7):
-            cores[-1][1+i,:,:,0] = m_mat[i]
-        cores[-1][8,:,:,0] = s_mat
+            cores[-1][1 + i, :, :, 0] = m_mat[i]
+        cores[-1][8, :, :, 0] = s_mat
         for i in range(7):
-        	cores[-1][9+i,:,:,0] = l_mat[i]
+            cores[-1][9 + i, :, :, 0] = l_mat[i]
 
         # define TT operator
         self.operator = TT(cores)
@@ -90,9 +91,7 @@ class TestSLIM(TestCase):
         operator = mdl.co_oxidation(self.order, self.k_ad_co)
 
         # compute error
-        err = np.amax(np.abs(self.operator.matricize()-operator.matricize()))
+        err = np.amax(np.abs(self.operator.matricize() - operator.matricize()))
 
         # check if error is smaller than tolerance
         self.assertLess(err, self.tol)
-   
-
