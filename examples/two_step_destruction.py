819067df1da38276dfd99363cac60bf2af1f13d0
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-05 16:18:19 +0100

    updated code

--- examples/two_step_destruction.py
+++ examples/two_step_destruction.py
@@ -11,16 +11,16 @@ References
       Networks, Catalytic Processes, Fluid Flows, and Brownian Dynamics", Freie Universität Berlin, 2017
 """
 
-import numpy as np
 from scikit_tt.tensor_train import TT
+import scikit_tt.tensor_train as tt
 import scikit_tt.models as mdl
-import scikit_tt.subfunctions as sf
-import scikit_tt.tools as tls
+import scikit_tt.solvers.ode as ode
+import scikit_tt.utils as utl
+import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters
 # ----------
-from scikit_tt.solvers import ODE
 
 m = 3
 step_sizes = [0.001] * 100 + [0.1] * 9 + [1] * 9
@@ -37,7 +37,7 @@ operator = mdl.two_step_destruction(1, 2, m).tt2qtt([[2] * m] + [[2] * (m + 1)] 
 # initial distribution in TT format and convert to QTT format
 # -----------------------------------------------------------
 
-initial_distribution = TT.zeros([2 ** m, 2 ** (m + 1), 2 ** m, 2 ** m], [1] * 4)
+initial_distribution = tt.zeros([2 ** m, 2 ** (m + 1), 2 ** m, 2 ** m], [1] * 4)
 initial_distribution.cores[0][0, -1, 0, 0] = 1
 initial_distribution.cores[1][0, -2, 0, 0] = 1
 initial_distribution.cores[2][0, 0, 0, 0] = 1
@@ -48,39 +48,37 @@ initial_distribution = TT.tt2qtt(initial_distribution, [[2] * m] + [[2] * (m + 1
 # initial guess in QTT format
 # ---------------------------
 
-initial_guess = TT.uniform([2] * (4 * m + 1), ranks=qtt_rank).ortho_right()
+initial_guess = tt.uniform([2] * (4 * m + 1), ranks=qtt_rank).ortho_right()
 
 # solve Markovian master equation in QTT format
 # ---------------------------------------------
 
 print('\nQTT approach')
 print('------------\n')
-with tls.Timer() as time:
-    solution, errors = ODE.implicit_euler_mals(operator, initial_distribution, initial_guess, step_sizes,
-                                               threshold=1e-7, max_rank=max_rank, compute_errors=True)
-print('CPU time ' + '.' * 22 + ' ' + str("%.2f" % time.elapsed) + 's')
-print('Maximum error ' + '.' * 17 + ' ' + str("%.2e" % np.amax(errors)))
+with utl.Timer() as time:
+    solution = ode.implicit_euler(operator, initial_distribution, initial_guess, step_sizes, tt_solver='mals',
+                                  threshold=1e-7, max_rank=max_rank)
+print('CPU time ' + '.' * 24 + ' ' + str("%.2f" % time.elapsed) + 's')
+
+# compute approximation errors
+errors = utl.errors_implicit_euler(operator, solution, step_sizes)
+print('Maximum error ' + '.' * 19 + ' ' + str("%.2e" % np.amax(errors)))
 
 # convert to TT and compute mean concentrations
 # ---------------------------------------------
 
 for i in range(len(solution)):
     solution[i] = TT.qtt2tt(solution[i], [m, m + 1, m, m])
-mean = sf.mean_concentrations(solution)
+mean = utl.mean_concentrations(solution)
 
 # plot mean concentrations
 # ------------------------
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams.update({'font.size': 14})
-plt.rcParams.update({'figure.autolayout': True})
+utl.plot_parameters()
 plt.plot(np.insert(np.cumsum(step_sizes), 0, 0), mean)
 plt.title('Mean concentrations', y=1.05)
 plt.xlabel(r'$t$')
 plt.ylabel(r'$\overline{x_i}(t)$')
 plt.axis([0, 2, 0, 2 ** (m + 1) - 2])
-plt.grid(which='major')
 plt.legend(['species ' + str(i) for i in range(1, 5)], loc=1)
 plt.show()
