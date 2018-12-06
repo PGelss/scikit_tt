7f612701d1d29687018c9a09789ec5db0e61eb7d
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-06 15:39:22 +0100

    revised code

--- examples/triple_well.py
+++ examples/triple_well.py
@@ -12,104 +12,125 @@ References
 """
 
 import numpy as np
-import scipy.io as spio
-import scikit_tt as tt
+from scikit_tt.tensor_train import TT
+import scikit_tt.tensor_train as tt
+import scikit_tt.solvers.evp as evp
+import scikit_tt.utils as utl
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+import os
+import scipy.io as io
 
 # parameters
 # ----------
 
-m = 500
+simulations = 500
 n = 50
 number_ev = 3
 
 # load data obtained by applying Ulam's method
 # --------------------------------------------
 
-tls.progress('\nLoad data',0,dots=39)
-ind = spio.loadmat("/storage/mi/gelssp/scikit_tt/data/TripleWell2D_500.mat")["indices"]  # load data
-tls.progress('Load data',100,dots=39)
+utl.progress('\nLoad data', 0, dots=39)
+directory = os.path.dirname(os.path.realpath(__file__))
+transitions = io.loadmat(directory + "/data/TripleWell2D_500.mat")["indices"]  # load data
+utl.progress('Load data', 100, dots=39)
 
 # construct TT operator
 # ---------------------
 
-cores = [None] * 2
-tls.progress('Construct operator', 0, dots=30)
+cores = []
 
 # find unique indices for transitions in the first dimension
-ind_unique, ind_inv , ind_counts= np.unique(ind[[0, 2], :], axis=1, return_inverse=True, return_counts=True)
+ind_unique, ind_inv, ind_counts = np.unique(transitions[[0, 2], :], axis=1, return_inverse=True, return_counts=True)
 rank = ind_unique.shape[1]
-cores[0] = np.zeros([1, n , n, rank])
+cores.append(np.zeros([1, n, n, rank]))
 for i in range(rank):
     cores[0][0, ind_unique[0, i] - 1, ind_unique[1, i] - 1, i] = 1
 
 # construct core for the second dimension
-cores[1] = np.zeros([rank, n,n, 1])
-for i in range(m * n * n):
-    cores[1][ind_inv[i], ind[1, i] - 1, ind[3, i] - 1, 0] += 1
+cores.append(np.zeros([rank, n, n, 1]))
+for i in range(transitions.shape[1]):
+    utl.progress('Construct operator', 100 * i / (transitions.shape[1] - 1), dots=30)
+    cores[1][ind_inv[i], transitions[1, i] - 1, transitions[3, i] - 1, 0] += 1
 
 # transpose and normalize operator
-operator = (1 / m) * tt.TT(cores).transpose()
-tls.progress('Construct operator', 100, dots=30)
+operator = (1 / simulations) * TT(cores).transpose()
 
 # approximate leading eigenfunctions of the Perron-Frobenius and Koopman operator
 # -------------------------------------------------------------------------------
-initial = tt.TT.ones(operator.row_dims, [1] * operator.order, ranks=11)
-eigenfunctions_pf, eigenvalues_pf = EVP.als(operator, initial, number_ev, 3)
-eigenfunctions_km, eigenvalues_km = EVP.als(operator.transpose(), initial, number_ev, 2)
+
+initial = tt.ones(operator.row_dims, [1] * operator.order, ranks=11)
+utl.progress('Approximate eigenfunctions in the TT format', 0, dots=5)
+eigenvalues_pf, eigenfunctions_pf = evp.als(operator, initial, number_ev, 3)
+utl.progress('Approximate eigenfunctions in the TT format', 50, dots=5)
+eigenvalues_km, eigenfunctions_km = evp.als(operator.transpose(), initial, number_ev, 2)
+utl.progress('Approximate eigenfunctions in the TT format', 100, dots=5)
 
 # compute exact eigenvectors
 # --------------------------
+
+utl.progress('Compute exact eigenfunctions in matrix format', 0)
 operator = operator.matricize()
 eigenvalues_pf_exact, eigenfunctions_pf_exact = np.linalg.eig(operator)
+utl.progress('Compute exact eigenfunctions in matrix format', 100)
 
-# plot eigenfunctions and table
+# convert results to matrices
+# ---------------------------
 
-plt.switch_backend('agg')
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams.update({'font.size': 10})
-plt.rcParams.update({'figure.autolayout': True})
-f, ax = plt.subplots(2, 3)
-f.suptitle('Eigenfunctions of the Perron-Frobenius and Koopman operator')
+eigenfunctions_mat = []
+eigenfunctions_mat_exact = []
+
+for i in range(number_ev):
+    eigenfunctions_mat.append(np.rot90(TT.full(eigenfunctions_pf[i])[:, :, 0, 0]))
+    eigenfunctions_mat_exact.append(np.rot90(np.real(eigenfunctions_pf_exact[:, i]).reshape(n, n)))
+    if i > 0:
+        eigenfunctions_mat[i] = eigenfunctions_mat[i] / np.amax(abs(eigenfunctions_mat[i]))
+        eigenfunctions_mat_exact[i] = eigenfunctions_mat_exact[i] / np.amax(abs(eigenfunctions_mat_exact[i]))
+for i in range(number_ev):
+    eigenfunctions_mat.append(np.rot90(TT.full(eigenfunctions_km[i])[:, :, 0, 0]))
+    eigenfunctions_mat[i + 3] = eigenfunctions_mat[i + 3] / np.amax(abs(eigenfunctions_mat[i + 3]))
+
+# compute errors
+# --------------
+
+err_eigval = []
+err_eigfun = []
+for i in range(number_ev):
+    err_eigval.append(np.abs(eigenvalues_pf_exact[i] - eigenvalues_pf[i]) / np.abs(eigenvalues_pf_exact[i]))
+    err_eigfun.append(np.amin([np.linalg.norm(eigenfunctions_mat_exact[i] - eigenfunctions_mat[i]),
+                               np.linalg.norm(eigenfunctions_mat_exact[i] + eigenfunctions_mat[i])]) / np.linalg.norm(
+        eigenfunctions_mat_exact[i]))
+
+# print table
+# -----------
+
 print('\n---------------------------------------------')
 print('k    lambda_k    err(lambda_k)    err(phi_k))')
 print('---------------------------------------------')
 for i in range(number_ev):
-    eigenfunction = np.rot90(tt.TT.full(eigenfunctions_pf[i])[:, :, 0, 0])
-    eigenfunction2 = np.rot90(np.real(eigenfunctions_pf_exact[:,i]).reshape(50,50))
-    if i > 0:
-        eigenfunction = eigenfunction / np.amax(abs(eigenfunction))
-        eigenfunction2 = eigenfunction2 / np.amax(abs(eigenfunction2))
-        im = ax[0, i].imshow(eigenfunction, cmap='jet', vmin=-1, vmax=1)
-    else:
-        im = ax[0, i].imshow(eigenfunction, cmap='jet', vmin=0, vmax=np.amax(abs(eigenfunction)))
-    divider = make_axes_locatable(ax[0, i])
-    cax = divider.append_axes('right', size='10%', pad=0.05)
-    f.colorbar(im, cax=cax, orientation='vertical')
-    ax[0, i].set_title(r'$\lambda$=' + str("%.4f" % np.abs(eigenvalues_pf_exact[i])))
-    ax[0, i].xaxis.set_ticklabels([])
-    ax[0, i].xaxis.set_ticks([])
-    ax[0, i].yaxis.set_ticklabels([])
-    ax[0, i].yaxis.set_ticks([])
-    err_eigval = np.abs(eigenvalues_pf_exact[i]-eigenvalues_pf[i])/np.abs(eigenvalues_pf_exact[i])
-    err_eigfun = np.amin([np.linalg.norm(eigenfunction2-eigenfunction),np.linalg.norm(eigenfunction2+eigenfunction)])/np.linalg.norm(eigenfunction2)
-    print(str(i+1)+'     '+str("%.4f" % np.abs(eigenvalues_pf_exact[i]))+'        '+str("%.2e" % err_eigval)+'         '+str("%.2e" % err_eigfun))
+    print(str(i + 1) + '     ' + str("%.4f" % np.abs(eigenvalues_pf_exact[i])) + '        ' + str(
+        "%.2e" % err_eigval[i]) + '         ' + str("%.2e" % err_eigfun[i]))
 print('---------------------------------------------\n')
-for i in range(number_ev):
-    eigenfunction = np.rot90(tt.TT.full(eigenfunctions_km[i])[:, :, 0, 0])
-    eigenfunction = eigenfunction / np.amax(abs(eigenfunction))
-    if i > 0:
-        im = ax[1, i].imshow(eigenfunction, cmap='jet', vmin=-1, vmax=1)
+
+# plot eigenfunctions and table
+
+utl.plot_parameters()
+
+f = plt.figure(figsize=plt.figaspect(0.65))
+
+for i in range(number_ev * 2):
+    ax = f.add_subplot(2, 3, i + 1)
+    if (i == 0) or (i == 3):
+        im = ax.imshow(eigenfunctions_mat[i], cmap='jet', vmin=0, vmax=np.amax(abs(eigenfunctions_mat[i])))
     else:
-        im = ax[1, i].imshow(eigenfunction, cmap='jet', vmin=0, vmax=np.amax(abs(eigenfunction)))
-    divider = make_axes_locatable(ax[1, i])
-    cax = divider.append_axes('right', size='10%', pad=0.05)
-    cbar = f.colorbar(im, cax=cax, orientation='vertical')
-    ax[1, i].xaxis.set_ticklabels([])
-    ax[1, i].xaxis.set_ticks([])
-    ax[1, i].yaxis.set_ticklabels([])
-    ax[1, i].yaxis.set_ticks([])
+        im = ax.imshow(eigenfunctions_mat[i], cmap='jet', vmin=-1, vmax=1)
+    cbar = f.colorbar(im, shrink=0.6, aspect=10)
+    cbar.ax.tick_params(labelsize=12)
+    if i < 3:
+        ax.set_title(r'$\lambda$=' + str("%.4f" % np.abs(eigenvalues_pf_exact[i])), fontsize=14)
+    ax.xaxis.set_ticklabels([])
+    ax.xaxis.set_ticks([])
+    ax.yaxis.set_ticklabels([])
+    ax.yaxis.set_ticks([])
+f.suptitle('Eigenfunctions of the Perron-Frobenius and Koopman operator')
 plt.show()
