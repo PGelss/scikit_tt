9ee857f73b2a0ad53d1ea989c5e85a8e3f30cd47
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-13 13:52:46 +0100

    renamed error routines

--- scikit_tt/utils.py
+++ scikit_tt/utils.py
@@ -9,6 +9,66 @@ import scikit_tt.tensor_train as tt
 import matplotlib.pyplot as plt
 
 
+def errors_impl_euler(operator, solution, step_sizes):
+    """Compute approximation errors of the implicit Euler method
+
+    Parameters
+    ----------
+    operator: instance of TT class
+        TT operator of the differential equation
+    solution: list of instances of TT class
+        approximate solution of the linear differential equation
+    step_sizes: list of floats
+        step sizes for the application of the implicit Euler method
+
+    Returns
+    -------
+    errors: list of floats
+        approximation errors
+    """
+
+    # define errors
+    errors = []
+
+    # compute relative approximation errors
+    for i in range(len(solution) - 1):
+        errors.append(
+            ((tt.eye(operator.row_dims) - step_sizes[i] * operator) @ solution[i + 1] - solution[i]).norm() /
+            solution[i].norm())
+
+    return errors
+
+
+def errors_trapezoidal(operator, solution, step_sizes):
+    """Compute approximation errors of the trapezoidal rule
+
+    Parameters
+    ----------
+    operator: instance of TT class
+        TT operator of the differential equation
+    solution: list of instances of TT class
+        approximate solution of the linear differential equation
+    step_sizes: list of floats
+        step sizes for the application of the implicit Euler method
+
+    Returns
+    -------
+    errors: list of floats
+        approximation errors
+    """
+
+    # define errors
+    errors = []
+
+    # compute relative approximation errors
+    for i in range(len(solution) - 1):
+        errors.append(((tt.eye(operator.row_dims) - 0.5 * step_sizes[i] * operator) @ solution[i + 1] -
+                       (tt.eye(operator.row_dims) + 0.5 * step_sizes[i] * operator) @ solution[i]).norm() /
+                      (tt.eye(operator.row_dims) + 0.5 * step_sizes[i] * operator) @ solution[i].norm())
+
+    return errors
+
+
 def header(title=None, subtitle=None):
     """Print scikit_tt header
 
@@ -32,81 +92,6 @@ def header(title=None, subtitle=None):
         print('    ' + subtitle)
     print(' ')
     print(' ')
-
-
-class Timer(object):
-    """Measure CPU time
-
-    Can be executed using the 'with' statement in order to measure the CPU time needed for calculations.
-    """
-
-    def __enter__(self):
-        self.start_time = _time.time()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.elapsed = _time.time() - self.start_time
-
-
-def progress(text, percent, dots=3):
-    """Show progress in percent
-
-    Print strings of the form, e.g., 'Running ... 10%' etc., without line breaks.
-
-    Parameters
-    ----------
-    text: string
-        string to print
-    percent: float
-        current progress
-    dots: int, optional
-        number of dots to print, default is 3
-    """
-
-    sys.stdout.write('\r' + text + ' ' + dots * '.' + ' ' + str("%.1f" % percent) + '%')
-
-    if percent == 100:
-        sys.stdout.write('\n')
-
-
-def plot_parameters(font_size=14):
-    """Customized plot parameters
-
-    Parameters
-    ----------
-    font_size: int
-        default font size for title, labels, etc.
-    """
-
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
-    plt.rcParams["mathtext.fontset"] = "cm"
-    plt.rcParams.update({'font.size': font_size})
-    plt.rcParams.update({'figure.autolayout': True})
-    plt.rcParams.update({'axes.grid': True})
-
-
-def unit_vector(dimension, index):
-    """Canonical unit vector
-
-    Return specific canonical unit vector in a given given dimension.
-
-    Parameters
-    ----------
-    dimension: int
-        dimension of the unit vector
-    index: int
-        position of the 1
-
-    Returns
-    -------
-    v: ndarray
-        unit vector
-    """
-    v = np.zeros(dimension)
-    v[index] = 1
-
-    return v
 
 
 def mean_concentrations(series):
@@ -145,61 +130,76 @@ def mean_concentrations(series):
     return mean
 
 
-def errors_implicit_euler(operator, solution, step_sizes):
-    """Compute approximation errors of the implicit Euler method
+def plot_parameters(font_size=14):
+    """Customized plot parameters
 
     Parameters
     ----------
-    operator: instance of TT class
-        TT operator of the differential equation
-    solution: list of instances of TT class
-        approximate solution of the linear differential equation
-    step_sizes: list of floats
-        step sizes for the application of the implicit Euler method
-
-    Returns
-    -------
-    errors: list of floats
-        approximation errors
+    font_size: int
+        default font size for title, labels, etc.
     """
 
-    # define errors
-    errors = []
-
-    # compute relative approximation errors
-    for i in range(len(solution) - 1):
-        errors.append(
-            ((tt.eye(operator.row_dims) - step_sizes[i] * operator) @ solution[i + 1] - solution[i]).norm() /
-            solution[i].norm())
-
-    return errors
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.rcParams["mathtext.fontset"] = "cm"
+    plt.rcParams.update({'font.size': font_size})
+    plt.rcParams.update({'figure.autolayout': True})
+    plt.rcParams.update({'axes.grid': True})
 
 
-def errors_trapezoidal_rule(operator, solution, step_sizes):
-    """Compute approximation errors of the trapezoidal rule
+def progress(text, percent, dots=3):
+    """Show progress in percent
+
+    Print strings of the form, e.g., 'Running ... 10%' etc., without line breaks.
 
     Parameters
     ----------
-    operator: instance of TT class
-        TT operator of the differential equation
-    solution: list of instances of TT class
-        approximate solution of the linear differential equation
-    step_sizes: list of floats
-        step sizes for the application of the implicit Euler method
+    text: string
+        string to print
+    percent: float
+        current progress
+    dots: int, optional
+        number of dots to print, default is 3
+    """
+
+    sys.stdout.write('\r' + text + ' ' + dots * '.' + ' ' + str("%.1f" % percent) + '%')
+
+    if percent == 100:
+        sys.stdout.write('\n')
+
+
+class Timer(object):
+    """Measure CPU time
+
+    Can be executed using the 'with' statement in order to measure the CPU time needed for calculations.
+    """
+
+    def __enter__(self):
+        self.start_time = _time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.elapsed = _time.time() - self.start_time
+
+
+def unit_vector(dimension, index):
+    """Canonical unit vector
+
+    Return specific canonical unit vector in a given given dimension.
+
+    Parameters
+    ----------
+    dimension: int
+        dimension of the unit vector
+    index: int
+        position of the 1
 
     Returns
     -------
-    errors: list of floats
-        approximation errors
+    v: ndarray
+        unit vector
     """
+    v = np.zeros(dimension)
+    v[index] = 1
 
-    # define errors
-    errors = []
-
-    # compute relative approximation errors
-    for i in range(len(solution) - 1):
-        errors.append(((tt.eye(operator.row_dims) - 0.5 * step_sizes[i] * operator) @ solution[i + 1] -
-                       (tt.eye(operator.row_dims) + 0.5 * step_sizes[i] * operator) @ solution[i]).norm() /
-                      (tt.eye(operator.row_dims) + 0.5 * step_sizes[i] * operator) @ solution[i].norm())
-
-    return errors
+    return v
