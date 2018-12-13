f52c6986eab641935cc532a7904ae7a6f8699998
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-13 11:02:23 +0100

    updated code

--- examples/example_mandy_fpu_1.py
+++ examples/example_mandy_fpu_1.py
@@ -8,11 +8,7 @@ from scikit_tt import mandy
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams.update({'font.size': 18})
-plt.rcParams.update({'figure.autolayout': True})
+
 
 
 D = scipy.io.loadmat("data/FPU_d10_m6000.mat")  # load data
@@ -32,51 +28,57 @@ repeats = 3
 
 # computations
 
-# m_mat = [0] * m_number_mat
-# T_mat = [0] * m_number_mat
-# E_mat = [0] * m_number_mat
-# for i in range(m_number_mat):
-#     m_tmp = m_start + i * m_step
-#     print("Running matrix-based MANDy for m="+str(m_tmp))
-#     for k in range(repeats):
-#         Xi_mat, time = mandy.mandy_basis_major_matrix(X[:,:m_tmp], Y[:,:m_tmp], psi, cpu_time=True)
-#         T_mat[i] = T_mat[i]+time.elapsed
-#     m_mat[i] = m_tmp
-#     T_mat[i] = T_mat[i]/repeats
-#     E_mat[i] = np.linalg.norm(Xi_mat.transpose() - Xi)/np.linalg.norm(Xi)
-# T_tt = np.zeros([exp_number, m_number_tt])
-# E_tt = np.zeros([exp_number, m_number_tt])
-# for j in range(exp_number):
-#     m_tt = [0] * m_number_tt
-#     for i in range(m_number_tt):
-#         m_tmp = m_start + i * m_step
-#         exp = exp_start + j * exp_step
-#         if exp == -11:
-#             print("Running MANDy for m="+str(m_tmp)+" and eps=0")
-#         else:
-#             print("Running MANDy for m=" + str(m_tmp) + " and eps=10^" + str(exp))
-#         for k in range(repeats):
-#             if exp == -11:
-#                 Xi_tt, time = mandy.mandy_basis_major(X[:,:m_tmp], Y[:,:m_tmp], psi, 0, cpu_time=True)
-#             else:
-#                 Xi_tt, time = mandy.mandy_basis_major(X[:, :m_tmp], Y[:, :m_tmp], psi, threshold=10 ** exp, cpu_time=True)
-#             T_tt[j,i] = T_tt[j,i]+time.elapsed
-#         m_tt[i] = m_tmp
-#         T_tt[j,i] = T_tt[j,i]/repeats
-#         Xi_tt = Xi_tt.full().reshape(np.prod(Xi_tt.row_dims[:-1]), Xi_tt.row_dims[-1], order='F')
-#         E_tt[j,i] = np.linalg.norm(Xi_tt - Xi) / np.linalg.norm(Xi)
+m_mat = [0] * m_number_mat
+T_mat = [0] * m_number_mat
+E_mat = [0] * m_number_mat
+for i in range(m_number_mat):
+    m_tmp = m_start + i * m_step
+    print("Running matrix-based MANDy for m="+str(m_tmp))
+    for k in range(repeats):
+        Xi_mat, time = mandy.mandy_basis_major_matrix(X[:,:m_tmp], Y[:,:m_tmp], psi, cpu_time=True)
+        T_mat[i] = T_mat[i]+time.elapsed
+    m_mat[i] = m_tmp
+    T_mat[i] = T_mat[i]/repeats
+    E_mat[i] = np.linalg.norm(Xi_mat.transpose() - Xi)/np.linalg.norm(Xi)
+T_tt = np.zeros([exp_number, m_number_tt])
+E_tt = np.zeros([exp_number, m_number_tt])
+for j in range(exp_number):
+    m_tt = [0] * m_number_tt
+    for i in range(m_number_tt):
+        m_tmp = m_start + i * m_step
+        exp = exp_start + j * exp_step
+        if exp == -11:
+            print("Running MANDy for m="+str(m_tmp)+" and eps=0")
+        else:
+            print("Running MANDy for m=" + str(m_tmp) + " and eps=10^" + str(exp))
+        for k in range(repeats):
+            if exp == -11:
+                Xi_tt, time = mandy.mandy_basis_major(X[:,:m_tmp], Y[:,:m_tmp], psi, 0, cpu_time=True)
+            else:
+                Xi_tt, time = mandy.mandy_basis_major(X[:, :m_tmp], Y[:, :m_tmp], psi, threshold=10 ** exp, cpu_time=True)
+            T_tt[j,i] = T_tt[j,i]+time.elapsed
+        m_tt[i] = m_tmp
+        T_tt[j,i] = T_tt[j,i]/repeats
+        Xi_tt = Xi_tt.full().reshape(np.prod(Xi_tt.row_dims[:-1]), Xi_tt.row_dims[-1], order='F')
+        E_tt[j,i] = np.linalg.norm(Xi_tt - Xi) / np.linalg.norm(Xi)
 # np.savez('data/example_mandy_fpu_1', m_mat=m_mat, T_mat=T_mat, E_mat=E_mat, m_tt=m_tt, T_tt=T_tt, E_tt=E_tt)
 
 
 # plots
 
-npzfile = np.load('data/example_mandy_fpu_1.npz') # load data
-m_mat = npzfile['m_mat']
-T_mat = npzfile['T_mat']
-E_mat = npzfile['E_mat']
-m_tt = npzfile['m_tt']
-T_tt = npzfile['T_tt']
-E_tt = npzfile['E_tt']
+# npzfile = np.load('data/example_mandy_fpu_1.npz') # load data
+# m_mat = npzfile['m_mat']
+# T_mat = npzfile['T_mat']
+# E_mat = npzfile['E_mat']
+# m_tt = npzfile['m_tt']
+# T_tt = npzfile['T_tt']
+# E_tt = npzfile['E_tt']
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams.update({'font.size': 18})
+plt.rcParams.update({'figure.autolayout': True})
 
 m_int = np.vstack([np.square(m_mat),np.array(m_mat),np.ones([1,len(m_mat)])]) # extrapolate CPU times
 T_int = T_mat.copy()
@@ -105,7 +107,7 @@ plt.title(r'CPU times for $d = 10$', y=1.03)
 plt.xlabel(r'$m$')
 plt.ylabel(r'$T / s$')
 plt.legend(loc=2, fontsize=12).get_frame().set_alpha(1)
-plt.savefig('example_fpu_1_a.pdf', dpi='figure', format='pdf')
+#plt.savefig('example_fpu_1_a.pdf', dpi='figure', format='pdf')
 plt.show()
 
 plt.figure(dpi=300)
@@ -126,5 +128,5 @@ plt.title(r'Relative errors for $d = 10$', y=1.03)
 plt.xlabel(r'$m$')
 plt.ylabel(r'$|| \Xi_{\textrm{exact}} - \Xi ||~/~|| \Xi_{\textrm{exact}} ||$')
 plt.legend(loc=1, fontsize=12).get_frame().set_alpha(1)
-plt.savefig('example_fpu_1_b.pdf', dpi='figure', format='pdf')
+#plt.savefig('example_fpu_1_b.pdf', dpi='figure', format='pdf')
 plt.show()
