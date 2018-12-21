bcbc9499717ebce5f939101ce8b0ec1dd01e97dc
Patrick Gelss <p.gelss@fu-berlin.de>
2018-12-21 12:48:07 +0100

    revised code

--- examples/co_oxidation.py
+++ examples/co_oxidation.py
@@ -1,72 +1,107 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""
+This is an example for the application of the TT format to a heterogeneous catalytic reaction system. For more details,
+see [1]_, [2]_, and [3]_.
+
+References
+----------
+.. [1] P. Gelß. "The Tensor-Train Format and Its Applications: Modeling and Analysis of Chemical Reaction
+       Networks, Catalytic Processes, Fluid Flows, and Brownian Dynamics", Freie Universität Berlin, 2017
+.. [2] P. Gelß, S. Matera, C. Schütte, "Solving the master equation without kinetic Monte Carlo: Tensor train
+       approximations for a CO oxidation model", Journal of Computational Physics 314 (2016) 489–502
+.. [3] P. Gelß, S. Klus, S. Matera, C. Schütte, "Nearest-neighbor interaction systems in the tensor-train format",
+       Journal of Computational Physics 341 (2017) 140-162
+"""
 
 import scikit_tt.tensor_train as tt
 import scikit_tt.models as mdl
-import scikit_tt.solvers.EVP as evp
-import scikit_tt.solvers.ODE as ODE
-import scikit_tt.tools as tls
+import scikit_tt.solvers.evp as evp
+import scikit_tt.solvers.ode as ode
+import scikit_tt.utils as utl
 import matplotlib.pyplot as plt
 
+utl.header(title='CO oxidation')
 
-def turn_over_frequency(distribution):
-    tt_left = [None] * distribution.order
-    tt_right = [None] * distribution.order
-    for i in range(distribution.order):
-        tt_left[i] = tt.TT.ones([1] * distribution.order, distribution.row_dims)
-        tt_right[i] = tt.TT.ones([1] * distribution.order, distribution.row_dims)
-        tt_left[i].cores[i][0, 0, :, 0] = [0, 0, 1]
-        tt_right[i].cores[i][0, 0, :, 0] = [0, 0, 1]
-        if i > 0:
-            tt_left[i].cores[i - 1][0, 0, :, 0] = [0, 1, 0]
-        else:
-            tt_left[i].cores[-1][0, 0, :, 0] = [0, 1, 0]
-        if i < distribution.order - 1:
-            tt_right[i].cores[i + 1][0, 0, :, 0] = [0, 1, 0]
-        else:
-            tt_right[i].cores[0][0, 0, :, 0] = [0, 1, 0]
-    tof = 0
-    k_de_CO2 = 1.7e5
-    for i in range(distribution.order):
-        tof = tof + (k_de_CO2 / distribution.order) * (tt_left[i] @ distribution).element([0]*distribution.order*2) + \
-              (k_de_CO2 / distribution.order) * (tt_right[i] @ distribution).element([0]*distribution.order*2)
-    return tof
-
-TOF = []
+# parameters
+order = 20
 p_CO_exp = [-4, -3.5, -3, -2.5, -2, -1.5, -1, -0.5, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.5, 2]
 
+# TT ranks for approximations
+#           - EVP -           |            - IEM -
+R = [3, 4, 5, 5, 6, 6, 9, 11] + [12]
 
-print('\n----------------------------------------------------------')
+# TODO: FIND FURTHER RANKS
+
+# define array for turn-over frequencies
+tof = []
+
+# compute solutions and print results
+# -----------------------------------
+
+print('----------------------------------------------------------')
 print('p_CO in atm    Method    TT ranks    Closeness    CPU time')
 print('----------------------------------------------------------')
 
-R = [3,3,4,7,8,8,10,10]
-for i in range(len(R)):
+# construct eigenvalue problems to find the stationary distributions
+# ------------------------------------------------------------------
+
+for i in range(8):
+    # construct and solve eigenvalue problem for current CO pressure
+    operator = mdl.co_oxidation(20, 10 ** (8 + p_CO_exp[i])).ortho_left().ortho_right()
+    initial_guess = tt.ones(operator.row_dims, [1] * operator.order, ranks=R[i]).ortho_left().ortho_right()
+    with utl.timer() as time:
+        eigenvalues, solution = evp.als(tt.eye(operator.row_dims) + operator, initial_guess, repeats=10, solver='eigs')
+        solution = (1 / solution.norm(p=1)) * solution
+
+    # compute turn-over frequency of CO2 desorption
+    tof.append(utl.two_cell_tof(solution, [2, 1], 1.7e5))
+
+    # print results
+    string_p_CO = '10^' + (p_CO_exp[i] >= 0) * '+' + str("%.1f" % p_CO_exp[i])
+    string_method = ' ' * 9 + 'EVP' + ' ' * 9
+    string_rank = (R[i] < 10) * ' ' + str(R[i]) + ' ' * 8
+    string_closeness = str("%.2e" % (operator @ solution).norm()) + ' ' * 6
+    string_time = (time.elapsed < 10) * ' ' + str("%.2f" % time.elapsed) + 's'
+    print(string_p_CO + string_method + string_rank + string_closeness + string_time)
+
+# apply adaptive step size scheme to the ODE to find the stationary distribution
+# ------------------------------------------------------------------------------
+
+for i in range(8, len(R)):
+
+    # integrate ODE to approximate stationary distribution
     operator = mdl.co_oxidation(20, 10 ** (8 + p_CO_exp[i]))
-    initial_guess = tt.TT.ones(operator.row_dims,[1]*operator.order, ranks=R[i]).ortho_right()
-    with tls.Timer() as time:
-        solution, eigenvalues = evp.als(tt.TT.eye(operator.row_dims)+operator, initial_guess, number_ev=1, repeats=20)
-        print(eigenvalues)
-        solution = (1/solution.norm(p=1))*solution
-    TOF.append(turn_over_frequency(solution).copy())
-    print('10^'+(p_CO_exp[i]>=0)*'+'+str("%.1f" % p_CO_exp[i])+'         EVP         '+(R[i]<10)*' '+str(R[i])+'        '+str("%.2e" % (operator@solution).norm())+(time.elapsed<10)*' '+'      '+str("%.2f" % time.elapsed) + 's')
+    initial_value = tt.unit(operator.row_dims, [1] * operator.order)
+    initial_guess = tt.ones(operator.row_dims, [1] * operator.order, ranks=R[i]).ortho_left().ortho_right()
+    with utl.timer() as time:
+        solution = ode.adaptive_step_size(operator, initial_value, initial_guess, 100)
 
-# R = [14, 15, 17, 18]
-# for i in range(3,len(R)):
-#     operator = mdl.CO_oxidation(20, 10 ** (8 + p_CO_exp[8+i]))
-#     initial_value = tt.TT.zeros(operator.row_dims, [1] * operator.order)
-#     for j in range(initial_value.order):
-#         initial_value.cores[j][0,1,0,0] = 1
-#     initial_guess = tt.TT.ones(operator.row_dims, [1] * operator.order, ranks=R[i]).ortho_right()
-#     with tls.Timer() as time:
-#         solution = ODE.adaptive_als(operator, initial_value, initial_guess, 10**-10, 10, repeats = 5)
-#     TOF.append(turn_over_frequency(solution[-1]).copy())
-#     print('10^+' + str("%.1f" % p_CO_exp[i+8]) + '         IEM         ' + (R[i] < 10) * ' ' + str(
-#         R[i]) + '        ' + str("%.2e" % (operator @ solution[-1]).norm()) + (time.elapsed < 10) * ' ' + '      ' + str(
-#         "%.2f" % time.elapsed) + 's')
+    # compute turn-over frequency of CO2 desorption
+    tof.append(utl.two_cell_tof(solution[-1], [2, 1], 1.7e5))
 
-print('------------------------------------------------')
+    # print results
+    string_p_CO = '10^' + (p_CO_exp[i] >= 0) * '+' + str("%.1f" % p_CO_exp[i])
+    string_method = ' ' * 9 + 'IEM' + ' ' * 9
+    string_rank = (R[i] < 10) * ' ' + str(R[i]) + ' ' * 8
+    string_closeness = str("%.2e" % (operator @ solution[-1]).norm()) + ' ' * 6
+    string_time = (time.elapsed < 10) * ' ' + str("%.2f" % time.elapsed) + 's'
+    print(string_p_CO + string_method + string_rank + string_closeness + string_time)
 
-# plt.plot(p_CO_exp[:len(R)],TOF)
-# plt.yscale('log')
-# plt.show()
+print('----------------------------------------------------------')
+
+# plot turn-over frequency
+# ------------------------
+
+utl.plot_parameters()
+plt.plot(p_CO_exp[:len(tof)], tof)
+plt.yscale('log')
+plt.title(r'CPU times for $d = 10$', y=1.03)
+plt.xlabel(r'$p_{\textrm{CO}}$')
+plt.ylabel(r'$\textrm{TOF}$')
+plt.title('Turn-over frequency', y=1.05)
+plt.xticks([-4, -3, -2, -1, 0, 1, 2],
+           (r'$10^{-4}$', r'$10^{-3}$', r'$10^{-2}$', r'$10^{-1}$', r'$10^{0}$', r'$10^{1}$', r'$10^{2}$'))
+plt.xlim(-4, 2)
+plt.ylim(10**-4, 10**6)
+plt.show()
