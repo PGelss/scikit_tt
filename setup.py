d713e2d590c965ed5c9fd2a36477dbc22c8fa581
Patrick Gelss <p.gelss@fu-berlin.de>
2019-01-12 23:14:06 +0100

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ metadata = dict(name='scikit_tt',
                 author='Patrick Gelss',
                 author_email='p.gelss@fu-berlin.de',
                 license='LGPLv3',
-                version='0.1',
+                version='1.0',
                 packages=find_packages(),
                 )
 
