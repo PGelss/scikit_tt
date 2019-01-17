a39a0d54a64ffbe3ef8affdb1a88dcf1364cd3c7
Patrick Gelss <p.gelss@fu-berlin.de>
2019-01-17 14:44:31 +0100

    added test for trapezoidal rule

--- tests/test_ode.py
+++ tests/test_ode.py
@@ -4,9 +4,6 @@ from unittest import TestCase
 import scikit_tt.models as mdl
 import scikit_tt.tensor_train as tt
 import scikit_tt.solvers.ode as ode
-import numpy as np
-import numpy.linalg as lin
-import scipy.sparse.linalg as splin
 
 
 class TestODE(TestCase):
@@ -18,13 +15,14 @@ class TestODE(TestCase):
         self.tol = 1e-3
 
         # set parameters
-        self.d = 2
+        self.d = 3
         self.k_1 = 1
         self.k_2 = 2
         self.m = 2
         self.rank = 4
         self.qtt_modes = [[2] * 6] * self.d
         self.step_sizes_signaling_cascade = [1] * 300
+        self.step_sizes_two_step_destruction = [0.001] * 100 + [0.1] * 9 + [1] * 19
 
         # construct operators
         self.operator_signaling_cascade = mdl.signaling_cascade(self.d).tt2qtt(self.qtt_modes, self.qtt_modes)
@@ -35,17 +33,39 @@ class TestODE(TestCase):
 
         # compute numerical solution of the ODE
         operator = self.operator_signaling_cascade
-        initial_value = tt.unit(operator.row_dims, [0]*operator.order)
-        initial_guess = tt.ones(operator.row_dims, [1]*operator.order, ranks=self.rank).ortho_right()
+        initial_value = tt.unit(operator.row_dims, [0] * operator.order)
+        initial_guess = tt.ones(operator.row_dims, [1] * operator.order, ranks=self.rank).ortho_right()
         step_sizes = self.step_sizes_signaling_cascade
         solution = ode.implicit_euler(operator, initial_value, initial_guess, step_sizes, progress=False)
 
         # compute norm of the derivatives at the final 10 time steps
         derivatives = []
         for i in range(10):
-            derivatives.append((operator @ solution[-i-1]).norm())
+            derivatives.append((operator @ solution[-i - 1]).norm())
 
-        # check if implicit Euler method converged to staionary distribution
+        # check if implicit Euler method converged to stationary distribution
         for i in range(10):
             self.assertLess(derivatives[i], self.tol)
 
+    def test_trapezoidal_rule(self):
+        """test for trapezoidal rule"""
+
+        # compute numerical solution of the ODE
+        operator = self.operator_two_step_destruction
+        initial_value = tt.zeros([2 ** self.m, 2 ** (self.m + 1), 2 ** self.m, 2 ** self.m], [1] * 4)
+        initial_value.cores[0][0, -1, 0, 0] = 1
+        initial_value.cores[1][0, -2, 0, 0] = 1
+        initial_value.cores[2][0, 0, 0, 0] = 1
+        initial_value.cores[3][0, 0, 0, 0] = 1
+        initial_guess = tt.ones(operator.row_dims, [1] * operator.order, ranks=self.rank).ortho_right()
+        step_sizes = self.step_sizes_two_step_destruction
+        solution = ode.trapezoidal_rule(operator, initial_value, initial_guess, step_sizes, progress=False)
+
+        # compute norm of the derivatives at the final 10 time steps
+        derivatives = []
+        for i in range(10):
+            derivatives.append((operator @ solution[-i - 1]).norm())
+
+        # check if trapezoidal rule converged to stationary distribution
+        for i in range(10):
+            self.assertLess(derivatives[i], self.tol)
