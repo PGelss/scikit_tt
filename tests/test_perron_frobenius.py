c281d5aa0035efbfadc488e814a2755f1a6f2ec1
Patrick Gelss <p.gelss@fu-berlin.de>
2019-01-24 11:44:58 +0100

    coarse-grained 3d data

--- tests/test_perron_frobenius.py
+++ tests/test_perron_frobenius.py
@@ -7,7 +7,7 @@ import os
 import scipy.io as io
 
 
-class TestSLIM(TestCase):
+class TestPF(TestCase):
 
     def setUp(self):
         """Consider triple- and quadruple well potentials for testing"""
@@ -19,6 +19,12 @@ class TestSLIM(TestCase):
         directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         self.transitions_2d = io.loadmat(directory + '/examples/data/TripleWell2D_500.mat')["indices"]
         self.transitions_3d = io.loadmat(directory + '/examples/data/QuadrupleWell3D_25x25x25_100.mat')["indices"]
+
+        # coarse-grain 3d data
+        for j in range(self.transitions_3d.shape[1]):
+            for i in range(self.transitions_3d.shape[0]):
+                self.transitions_3d[i,j] = np.ceil(self.transitions_3d[i,j]/5)
+
 
     def test_perron_frobenius_2d(self):
         """test for perron_frobenius_2d"""
@@ -43,14 +49,14 @@ class TestSLIM(TestCase):
         """test for perron_frobenius_3d"""
     
         # construct transition operator in TT format
-        operator = pf.perron_frobenius_3d(self.transitions_3d, [25, 25, 25], 100)
+        operator = pf.perron_frobenius_3d(self.transitions_3d, [5,5,5], 12500)
 
         # construct full operator
-        operator_full = np.zeros([25,25,25,25,25,25])
+        operator_full = np.zeros([5,5,5,5,5,5])
         for i in range(self.transitions_3d.shape[1]):
             [x_1, y_1, z_1, x_2, y_2, z_2] = self.transitions_3d[:, i] - 1
             operator_full[x_2, y_2, z_2, x_1, y_1, z_1] += 1
-        operator_full *= 1/100
+        operator_full *= 1/12500
 
         # compute error
         error = np.abs(operator.full()-operator_full).sum()
