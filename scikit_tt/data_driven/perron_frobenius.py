5a85d72f3d28d53532387e5e2f51c2ef49af6397
Patrick Gelss <p.gelss@fu-berlin.de>
2019-01-27 00:42:31 +0100

    use axis parameter in unique()

--- scikit_tt/data_driven/perron_frobenius.py
+++ scikit_tt/data_driven/perron_frobenius.py
@@ -32,17 +32,15 @@ def perron_frobenius_2d(transitions, states, simulations):
     .. [1] P. Gelß. "The Tensor-Train Format and Its Applications: Modeling and Analysis of Chemical Reaction
            Networks, Catalytic Processes, Fluid Flows, and Brownian Dynamics", Freie Universität Berlin, 2017
     """
-
+    
     # find unique indices for transitions in the first dimension
-    flat_indices = np.ravel_multi_index(transitions[[0, 2], :] - 1, (states[0], states[0]))
-    [ind_unique, ind_inv] = np.unique(flat_indices, return_inverse=True)
-    ind_unique = np.array(np.unravel_index(ind_unique, (states[0], states[0])))
+    [ind_unique, ind_inv] = np.unique(transitions[[0, 2], :], axis=1, return_inverse=True)
     rank = ind_unique.shape[1]
 
     # construct core for the first dimension
     cores = [np.zeros([1, states[0], states[0], rank])]
     for i in range(rank):
-        cores[0][0, ind_unique[0, i], ind_unique[1, i], i] = 1
+        cores[0][0, ind_unique[0, i] - 1, ind_unique[1, i] - 1, i] = 1
 
     # construct core for the second dimension
     cores.append(np.zeros([rank, states[1], states[1], 1]))
@@ -50,10 +48,9 @@ def perron_frobenius_2d(transitions, states, simulations):
         cores[1][ind_inv[i], transitions[1, i] - 1, transitions[3, i] - 1, 0] += 1
 
     # transpose and normalize operator
-    operator = (np.true_divide(1, simulations)) * TT(cores).transpose()
+    operator = np.true_divide(1, simulations) * TT(cores).transpose()
 
     return operator
-
 
 def perron_frobenius_3d(transitions, states, simulations):
     """TT approximation of the Perron-Frobenius operator in 3D
@@ -83,20 +80,16 @@ def perron_frobenius_3d(transitions, states, simulations):
     """
 
     # find unique indices for transitions in the first dimension
-    flat_indices = np.ravel_multi_index(transitions[[0, 3], :] - 1, (states[0], states[0]))
-    [ind_1_unique, ind_1_inv] = np.unique(flat_indices, return_inverse=True)
-    ind_1_unique = np.array(np.unravel_index(ind_1_unique, (states[0], states[0])))
+    [ind_1_unique, ind_1_inv] = np.unique(transitions[[0, 3], :], axis=1, return_inverse=True)
     rank_1 = ind_1_unique.shape[1]
 
     # construct core for the first dimension
     cores = [np.zeros([1, states[0], states[0], rank_1])]
     for i in range(rank_1):
-        cores[0][0, ind_1_unique[0, i], ind_1_unique[1, i], i] = 1
+        cores[0][0, ind_1_unique[0, i] - 1, ind_1_unique[1, i] - 1, i] = 1
 
     # find unique indices for transitions in the third dimension
-    flat_indices = np.ravel_multi_index(transitions[[2, 5], :] - 1, (states[2], states[2]))
-    [ind_2_unique, ind_2_inv] = np.unique(flat_indices, return_inverse=True)
-    ind_2_unique = np.array(np.unravel_index(ind_2_unique, (states[2], states[2])))
+    [ind_2_unique, ind_2_inv] = np.unique(transitions[[2, 5], :], axis=1, return_inverse=True)
     rank_2 = ind_2_unique.shape[1]
 
     # list entry for the second core
@@ -105,7 +98,7 @@ def perron_frobenius_3d(transitions, states, simulations):
     # construct core for the third dimension
     cores.append(np.zeros([rank_2, states[2], states[2], 1]))
     for i in range(rank_2):
-        cores[2][i, ind_2_unique[0, i], ind_2_unique[1, i], 0] = 1
+        cores[2][i, ind_2_unique[0, i] - 1, ind_2_unique[1, i] - 1, 0] = 1
 
     # construct core for the second dimension
     cores[1] = np.zeros([rank_1, states[1], states[1], rank_2])
