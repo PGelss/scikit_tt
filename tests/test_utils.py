59fdf3ddfe5d26b603d57082af6699fcc1ecffb0
Patrick Gelss <p.gelss@fu-berlin.de>
2020-11-07 19:14:40 +0100

    added test for truncated_svd

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import scipy.linalg as lin
 import scikit_tt.utils as utl
 from unittest import TestCase
 import sys
@@ -9,6 +10,14 @@ import time
 
 
 class TestUtils(TestCase):
+
+    def setUp(self):
+        """construct random matrix for test of trncated_svd"""
+
+        self.m = np.random.rand(50,100)
+        self.threshold = 1e-1
+        self.max_rank = 15
+        self.tol = 1e-12
 
     def test_header(self):
         """test header"""
@@ -39,3 +48,30 @@ class TestUtils(TestCase):
         with utl.timer() as timer:
             time.sleep(1)
         self.assertLess(np.abs(timer.elapsed - 1), 0.1)
+
+    def test_truncated_svd(self):
+        """test truncated_svd"""
+
+        # decompose m using different parameters
+        u_1, s_1, v_1 = lin.svd(self.m, full_matrices=False)
+        u_2, s_2, v_2 = utl.truncated_svd(self.m)
+        u_3, s_3, v_3 = utl.truncated_svd(self.m, threshold=self.threshold)
+        u_4, s_4, v_4 = utl.truncated_svd(self.m, max_rank=self.max_rank)
+
+        # no truncation
+        self.assertLess(np.linalg.norm(u_1-u_2), self.tol)
+        self.assertLess(np.linalg.norm(s_1-s_2), self.tol)
+        self.assertLess(np.linalg.norm(v_1-v_2), self.tol)
+
+        # relative truncation
+        self.assertLess(np.linalg.norm(u_1[:,:len(s_3)]-u_3), self.tol)
+        self.assertLess(np.linalg.norm(s_1[:len(s_3)]-s_3), self.tol)
+        self.assertLess(np.linalg.norm(v_1[:len(s_3),:]-v_3), self.tol)
+
+        self.assertGreater(s_3[-1]/s_1[0], self.threshold)
+        self.assertLess(s_1[len(s_3)]/s_1[0], self.threshold)
+
+        # absolute truncation
+        self.assertLess(np.linalg.norm(u_1[:,:self.max_rank]-u_4), self.tol)
+        self.assertLess(np.linalg.norm(s_1[:self.max_rank]-s_4), self.tol)
+        self.assertLess(np.linalg.norm(v_1[:self.max_rank,:]-v_4), self.tol)
